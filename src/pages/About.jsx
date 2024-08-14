990d369cfc541ffe4e3112e4a6a6e73dc894946e
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-14 19:54:49 +0800

    complete about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,43 @@
-// import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6-xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Real State</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
+        dignissim justo, vitae molestie quam. Nunc convallis neque vel libero
+        suscipit faucibus. Cras eget arcu libero. Maecenas viverra dui dolor,
+        sit amet tempus est porttitor ut. Pellentesque condimentum blandit arcu.
+        Integer ultricies magna non dolor dignissim, non lacinia erat tempus.
+        Aenean bibendum arcu quis orci congue, a malesuada ipsum laoreet. Mauris
+        vehicula sollicitudin porttitor. Suspendisse suscipit blandit dolor, non
+        venenatis dolor. Ut pharetra ultrices velit, ac congue justo porttitor
+        id. Aenean efficitur eleifend elementum. Praesent convallis vulputate
+        nibh, ac egestas lacus venenatis vel.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
+        dignissim justo, vitae molestie quam. Nunc convallis neque vel libero
+        suscipit faucibus. Cras eget arcu libero. Maecenas viverra dui dolor,
+        sit amet tempus est porttitor ut. Pellentesque condimentum blandit arcu.
+        Integer ultricies magna non dolor dignissim, non lacinia erat tempus.
+        Aenean bibendum arcu quis orci congue, a malesuada ipsum laoreet. Mauris
+        vehicula sollicitudin porttitor. Suspendisse suscipit blandit dolor, non
+        venenatis dolor. Ut pharetra ultrices velit, ac congue justo porttitor
+        id. Aenean efficitur eleifend elementum. Praesent convallis vulputate
+        nibh, ac egestas lacus venenatis vel.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
+        dignissim justo, vitae molestie quam. Nunc convallis neque vel libero
+        suscipit faucibus. Cras eget arcu libero. Maecenas viverra dui dolor,
+        sit amet tempus est porttitor ut. Pellentesque condimentum blandit arcu.
+        Integer ultricies magna non dolor dignissim, non lacinia erat tempus.
+        Aenean bibendum arcu quis orci congue, a malesuada ipsum laoreet. Mauris
+        vehicula sollicitudin porttitor. Suspendisse suscipit blandit dolor, non
+        venenatis dolor. Ut pharetra ultrices velit, ac congue justo porttitor
+        id. Aenean efficitur eleifend elementum. Praesent convallis vulputate
+        nibh, ac egestas lacus venenatis vel.
+      </p>
+    </div>
+  );
 }
